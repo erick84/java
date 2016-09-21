24eb802201a6ae6bf4633175f4a8a1684220958b
MAYORQUIN84 <erickmayorquin1984@gmail.com>
2016-09-20 23:19:27 -0500

    agregando
    edad

--- Holamundo/src/holamundo.java
+++ Holamundo/src/holamundo.java
@@ -5,13 +5,17 @@ import java.io.InputStreamReader;
 public class holamundo {
 
     public static void main (String [] args) throws IOException {
-       String nombre;
+       String nombre, edad;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("hola mundo.");
         System.out.println("como te llamas");
         nombre = br.readLine();
         System.out.println("hola " +nombre);
+        System.out.println("cual es tu edad");
+        edad = br.readLine();
+        System.out.println ("tu edad es "+edad);
+
 
 
 
